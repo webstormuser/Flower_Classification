8b7e236d25117a13488fdbc3fa0ee8a309ef92c2
Ashwini <kakdeashwini12@gmail.com>
2024-02-03 15:24:39 +0530

    requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Flower_Classification
+
+* Step1 : Create environment for project 
+     python -m venv flowers
+* Step2 : Activate environmnet 
+    .\flowers\Scripts\activate
+
+* Step3 : Install requirements
+    pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Flower_classification",
+    version="0.0.1",
+    author="Ashwini",
+    author_email="kakdeashwini12@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
